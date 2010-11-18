1fbc1de7b276055f38582d26a8b5698514959bd6
Scott Davis <jetviper21@gmail.com>
2010-11-18 09:54:17 -0500

    removed distinct

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
   def search
-    @items = Item.select('DISTINCT(name) as name').limit('30').where(["Lower(name) LIKE ?", "%#{params[:name].downcase}%"])
+    @items = Item.select('name').limit('30').where(["Lower(name) LIKE ?", "%#{params[:name].downcase}%"])
     render :json => @items.to_json(:only => :name)
   end
   
